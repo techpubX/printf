53d77fbee534cbecef6cd6b36be715d6fb1f1f8e
Omondi <89958289+techpubX@users.noreply.github.com>
2022-08-11 20:04:29 +0300

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -9,28 +9,5 @@
  */
 int _putchar(char c)
 {
-	return (buffer(c));
-}
-
-/**
- * buffer - Save the character in a buffer
- * @c: Character
- *
- * Return: 1
- **/
-int buffer(char c)
-{
-	static char buffering[1024];
-	static int i;
-
-	if (c == -1 || i == 1024)
-	{
-		write(1, buffering, i);
-		i = 0;
-	}
-
-	if (c != -1)
-		buffering[i++] = c;
-
-	return (1);
+	return (write(1, &c, 1));
 }
